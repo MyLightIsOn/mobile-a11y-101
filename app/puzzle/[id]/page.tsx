810622425be68c1c5a68a19b5dc2f761c41bdcd4
Lawrence Moore <thelawrencemoore@gmail.com>
2025-04-01 16:43:49 -0700

    Refactor puzzle link component and add puzzle page.
    Reintroduced `PuzzleLink` with navigation support and improved styling. Added a new dynamic puzzle page at `app/puzzle/[id]/page.tsx` with placeholder content for future enhancements.

---
+++ app/puzzle/[id]/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className={"bg-black text-center p-4"}>
+      <h1 className={"font-bold text-2xl pb-2"}>Puzzle 1:</h1>
+      <h2 className={"text-xl"}>Heading Into The Shadows</h2>
+      <h3 className={"opacity-0"}>Not this one</h3>
+      <h3 className={"opacity-0"}>Not this one either</h3>
+      <h3 className={"opacity-0"}>Nope</h3>
+      <h3 className={"opacity-0"}>Getting closer</h3>
+      <h3 className={"opacity-0"}>Almost</h3>
+      <h3 className={"opacity-0"}>The password is "Headings Help"</h3>
+      <h3 className={"opacity-0"}>The password is "You missed it"</h3>
+      <h3 className={"opacity-0"}>The password is "Turn back around"</h3>
+      <h3 className={"opacity-0"}>The password is "You're way off"</h3>
+    </div>
+  );
+}
+
+export default Page;
--- components/puzzle-link.tsx
+++ components/puzzle-link.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 function PuzzleLink({ number }: { number: number }) {
   return (
-    <div className="w-1/2 h-1/5 md:w-1/5 md:h-1/2 bg-black dark:bg-white border-2 border-black">
-      <div
+    <div className="w-1/2 h-1/5 md:w-1/5 md:h-1/2 bg-black dark:bg-white border-2 border-black justify-center items-center text-center">
+      <a
+        href={`puzzle/${number}`}
         className={
-          "h-full w-full flex items-center justify-center text-6xl font-bold"
+          "h-full w-full block flex items-center justify-center text-6xl font-bold dark:bg-white dark:hover:bg-black dark:hover:text-white hover:underline"
         }
       >
         {number}
-      </div>
+      </a>
     </div>
   );
 }
