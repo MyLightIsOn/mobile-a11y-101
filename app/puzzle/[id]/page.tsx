813ff387f57b0d72728e404c1f2f943bbffac188
Lawrence Moore <thelawrencemoore@gmail.com>
2025-04-01 16:53:30 -0700

    Enhance hidden text elements with non-selectable properties
    Added `select-none` and `pointer-events-none` to hidden `<h3>` elements to prevent text selection and interaction. This ensures these elements remain completely inaccessible during user interactions.

--- app/puzzle/[id]/page.tsx
+++ app/puzzle/[id]/page.tsx
@@ -5,15 +5,29 @@ function Page() {
     <div className={"bg-black text-center p-4"}>
       <h1 className={"font-bold text-2xl pb-2"}>Puzzle 1:</h1>
       <h2 className={"text-xl"}>Heading Into The Shadows</h2>
-      <h3 className={"opacity-0"}>Not this one</h3>
-      <h3 className={"opacity-0"}>Not this one either</h3>
-      <h3 className={"opacity-0"}>Nope</h3>
-      <h3 className={"opacity-0"}>Getting closer</h3>
-      <h3 className={"opacity-0"}>Almost</h3>
-      <h3 className={"opacity-0"}>The password is "Headings Help"</h3>
-      <h3 className={"opacity-0"}>The password is "You missed it"</h3>
-      <h3 className={"opacity-0"}>The password is "Turn back around"</h3>
-      <h3 className={"opacity-0"}>The password is "You're way off"</h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        Not this one
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        Not this one either
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>Nope</h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        Getting closer
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>Almost</h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        The password is "Headings Help"
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        You missed i
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        Turn back around
+      </h3>
+      <h3 className={"opacity-0 select-none pointer-events-none"}>
+        You're way off
+      </h3>
     </div>
   );
 }
