7b1ff6226b2903f4da2c50b02a4fa23fd686d3b0
Lawrence Moore <thelawrencemoore@gmail.com>
2025-04-01 17:06:12 -0700

    Improve hidden heading accessibility with size and overflow fix
    Added `w-0`, `h-0`, and `overflow-hidden` classes to hidden `<h3>` elements for better semantic structure and to fully exclude them from layout and rendering effects. These changes will enhance the effect of not being able to see the screen

--- app/puzzle/[id]/page.tsx
+++ app/puzzle/[id]/page.tsx
@@ -5,27 +5,67 @@ function Page() {
     <div className={"bg-black text-center p-4"}>
       <h1 className={"font-bold text-2xl pb-2"}>Puzzle 1:</h1>
       <h2 className={"text-xl"}>Heading Into The Shadows</h2>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         Not this one
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         Not this one either
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>Nope</h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
+        Nope
+      </h3>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         Getting closer
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>Almost</h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
+        Almost
+      </h3>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         The password is "Headings Help"
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
-        You missed i
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
+        You missed it
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         Turn back around
       </h3>
-      <h3 className={"opacity-0 select-none pointer-events-none"}>
+      <h3
+        className={
+          "opacity-0 select-none pointer-events-none w-0 h-0 overflow-hidden"
+        }
+      >
         You're way off
       </h3>
     </div>
