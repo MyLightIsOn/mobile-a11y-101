f80bfde1292b9765ba642174361642fcf3b17609
Lawrence Moore <thelawrencemoore@gmail.com>
2025-04-05 14:47:53 -0700

    Add PuzzleFooter component with dialog and back link support
    Introduced a new PuzzleFooter component to display a fixed-position dialog helper and an optional back link. This enhances navigational support and interaction capabilities in the footer area.

---
+++ components/puzzle-footer.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import GhostHelper from "@/components/ghost-helper";
+
+interface Props {
+  dialogContent: {
+    title: string;
+    description: ReactNode;
+  };
+  url?: string;
+}
+
+function PuzzleFooter({ dialogContent, url }: Props) {
+  return (
+    <>
+      <div className={"fixed bottom-0 right-0"}>
+        <GhostHelper dialogContent={dialogContent} />
+      </div>
+      {url && (
+        <div className={"fixed bottom-10 left-5"}>
+          <a
+            className={
+              "text-white focus:underline hover:underline p-4 focus:outline-dashed focus:outline-3 outline-white"
+            }
+            href={url}
+          >
+            <i className="arrow arrow-left text-white"></i> Back
+          </a>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PuzzleFooter;
