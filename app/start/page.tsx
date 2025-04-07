625446e8f1280f67baf8138f2ddedabe5357e84e
Lawrence Moore <thelawrencemoore@gmail.com>
2025-04-07 13:51:14 -0700

    Disable overlay in start page for simplified UI.
    The commented-out overlay was initially intended to increase the difficulty but has now been disabled for a cleaner and more user-friendly interface. This change ensures better visibility of the puzzle components.

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -4,8 +4,8 @@ import PuzzleLink from "@/components/puzzle-link";
 function Page() {
   return (
     <div className="h-[90vh] w-screen grid grid-cols-2 grid-rows-3 gap-1 card-container">
-      <div className={"absolute top-0 left-0 w-screen h-screen z-10"} />
-
+      {/*Uncomment overlay to raise difficult*/}`
+      {/*<div className={"absolute top-0 left-0 w-screen h-screen z-10"} />*/}`
       {Array.from({ length: 6 }).map((_, index) => (
         <PuzzleLink key={`puzzle-${index}`} number={index} />
       ))}
