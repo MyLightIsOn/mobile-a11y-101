89b54024ad53b6c07211cb642481101342cd0818
Lawrence Moore <thelawrencemoore@gmail.com>
2025-05-10 15:30:29 -0700

    Refactor Puzzle 3 interactions and add portrait animations
    Updated Puzzle 3 to improve interactions by wrapping portraits with divs, enabling better click handling, and introducing smooth rotation animations via CSS. Adjusted descriptions, fixed typos, and streamlined logic for setting puzzle completion. These changes enhance the user experience and address previous layout inconsistencies.

--- app/puzzle/3/page.tsx
+++ app/puzzle/3/page.tsx
@@ -20,9 +20,9 @@ const puzzleSolvedContent = {
   puzzleNumber: 3,
   description: (
     <p className={"text-left"}>
-      Alternative text is what is the description of an image that is read to a
-      screen reader. When we use images, it is essential to give them good
-      alternative text.
+      Alternative text is the description of an image that is read to a screen
+      reader. When we use images, it is essential to give them good alternative
+      text.
     </p>
   ),
 };
@@ -40,83 +40,91 @@ const Puzzle3 = () => {
     return () => clearInterval(id);
   }, []);
 
+  useEffect(() => {
+    if (fixedPainting.length === 4) {
+      setPuzzleSolved(true);
+      localStorage.setItem("puzzle_3_time", elapsedTime.toString());
+      localStorage.setItem("puzzle_3_complete", "true");
+    }
+  }, [fixedPainting]);
+
   return (
     <div className="bg-black text-white min-h-screen w-screen text-center p-4 pb-30">
       <h1 className="text-2xl font-bold pb-2">
         Puzzle 3: The Forgotten Gallery
       </h1>
-      <h2 className="text-xl pb-4">
-        The portraits are quiet. But one voice lingers...
-      </h2>
+      <h2 className="text-xl pb-4">Speaking Pictures</h2>
 
-      <p className={"sr-only"}>
-        You step into a cold, breezy with no windows. The walls are lined with
-        dusty portraits. Their eyes seem to follow you. The frames are aged, the
-        faces silent, but one looks like they have a secret.
+      <p className={"sr-only -top-96 -left-96 overflow-hidden"}>
+        You step into a cold, breezy room with no windows. The walls are lined
+        with dusty portraits. Their eyes seem to follow you. The frames are
+        aged, the faces silent, but one looks like they have a secret.
       </p>
 
       <div className="grid grid-cols-2 gap-4 max-w-3xl mx-auto">
-        <img
-          src="/husband.webp"
-          alt="A faded portrait of a stern-looking husband. On the back of the painting it says: 'Taxes were the real horror.'"
-          className={`w-full h-auto ${fixedPainting.includes("dad") ? "" : "rotate-8 relative left-2"}`}
+        <div
           onClick={() => {
             setFixedPainting((prevFixedPainting) => [
               ...prevFixedPainting,
               "dad",
             ]);
           }}
-        />
-        <img
-          src="/wife.webp"
-          alt="An elegant woman in a dark gown. On the back: 'Don't forget to water the plants.'"
-          className={`w-full h-auto ${fixedPainting.includes("mom") ? "" : "-rotate-4 relative right-4"}`}
+        >
+          <img
+            src="/husband.webp"
+            alt="A faded portrait of a stern-looking husband. On the back of the painting it says: 'Taxes were the real horror.'"
+            className={`w-full h-auto relative portrait ${fixedPainting.includes("dad") ? "-rotate-0 left-0" : "rotate-6 left-2"}`}
+          />
+        </div>
+        <div
           onClick={() => {
             setFixedPainting((prevFixedPainting) => [
               ...prevFixedPainting,
               "mom",
             ]);
           }}
-        />
+        >
+          <img
+            src="/wife.webp"
+            alt="An elegant woman in a dark gown. On the back: 'Don't forget to water the plants.'"
+            className={`w-full h-auto relative portrait ${fixedPainting.includes("mom") ? "-rotate-0 left-0" : "-rotate-6 -left-4"}`}
+          />
+        </div>
 
-        <PuzzleComplete
-          dialogContent={puzzleSolvedContent}
-          puzzleSolved={puzzleSolved}
-          delay={fixedPainting.length !== 3}
-          buttonText={
-            "A young girl with a mischievous smile. On the back: 'Our portraits are a mess. Double tap on each of to fix them. Then fix mine last."
-          }
+        <div
+          onClick={() => {
+            setFixedPainting((prevFixedPainting) => [
+              ...prevFixedPainting,
+              "kid",
+            ]);
+          }}
         >
           <img
             src="/daughter.webp"
-            alt="A young girl with a mischievous smile. On the back: 'Our portraits are a mess. Double tap on each of to fix them. Then fix mine last."
-            className={`w-full h-auto ${fixedPainting.includes("kid") ? "" : "-rotate-4"}`}
-            onClick={() => {
-              if (fixedPainting.length === 3) {
-                setFixedPainting((prevFixedPainting) => [
-                  ...prevFixedPainting,
-                  "kid",
-                ]);
-                setPuzzleSolved(true);
-                localStorage.setItem("puzzle_3_time", elapsedTime.toString());
-                localStorage.setItem("puzzle_3_complete", "true");
-              }
-            }}
+            alt="A young girl with a mischievous smile. On the back: 'Our portraits are a mess. Double tap on each one to fix them."
+            className={`w-full h-auto relative portrait ${fixedPainting.includes("kid") ? "-rotate-0 left-0" : "-rotate-4 left-3"}`}
           />
-        </PuzzleComplete>
+        </div>
 
-        <img
-          src="/dog.webp"
-          alt="A shaggy, but happy dog. On the back: 'Woof! Woof!'"
-          className={`w-full h-auto ${fixedPainting.includes("dog") ? "" : "rotate-6 relative -left-2"}`}
+        <div
           onClick={() => {
             setFixedPainting((prevFixedPainting) => [
               ...prevFixedPainting,
               "dog",
             ]);
           }}
-        />
+        >
+          <img
+            src="/dog.webp"
+            alt="A shaggy, but happy dog. On the back: 'Woof! Woof!'"
+            className={`w-full h-auto relative portrait ${fixedPainting.includes("dog") ? "-rotate-0 left-0" : "rotate-3 -left-2"}`}
+          />
+        </div>
       </div>
+      <PuzzleComplete
+        dialogContent={puzzleSolvedContent}
+        puzzleSolved={puzzleSolved}
+      />
       <PuzzleFooter dialogContent={dialogContent} url={"/start"} />
     </div>
   );
